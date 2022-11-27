65c1d795c5ba8bf4cb08a90cc8cba6bc5ecb175c
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-28 04:43:53 +0600

    API added for checking if a user is a buyer

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isAdmin: user?.role === "Admin" });
     });
+
+    // API for checking if a user is a Buyer
+    app.get("/users/buyer/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "Buyer" });
+    });
   } finally {
   }
 }
