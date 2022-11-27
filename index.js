83cdfd630c89241c379861f68cd3fa74f73b0adc
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-11-28 04:43:14 +0600

    API added for checking if a user is an admin

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    // API for checking if a user is an Admin
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "Admin" });
+    });
   } finally {
   }
 }
